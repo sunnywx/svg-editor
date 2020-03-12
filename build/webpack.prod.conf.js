e00467f01928711fb70ea1349420c51bfcb29b07
sunnyw <sunnywang@yunify.com>
2020-03-12 21:49:00 +0800

    chore: Replace uglifyjs plugin with terser plugin
    remove package-lock.json, using yarn.lock
    add precommit hook

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,111 +1,110 @@
-const path = require('path');
-const utils = require('./utils');
-const webpack = require('webpack');
-const config = require('../config');
-const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.conf');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const utils = require("./utils");
+const webpack = require("webpack");
+const config = require("../config");
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.conf");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const env = config.build.env;
 
 let webpackConfig = merge(baseWebpackConfig, {
-    mode: 'production',
-    module: {
-        rules: utils.styleLoaders({
-            sourceMap: config.build.productionSourceMap
-        })
+  mode: "production",
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.build.productionSourceMap
+    })
+  },
+  devtool: config.build.productionSourceMap ? "#source-map" : false,
+  output: {
+    path: config.build.assetsRoot,
+    filename: utils.assetsPath("js/[name].[chunkhash].js"),
+    chunkFilename: utils.assetsPath("js/[name].[chunkhash].js")
+  },
+  optimization: {
+    // split vendor js into its own file
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "initial",
+          name: "vendor"
+        }
+      }
     },
-    devtool: config.build.productionSourceMap ? '#source-map' : false,
-    output: {
-        path: config.build.assetsRoot,
-        filename: utils.assetsPath('js/[name].[chunkhash].js'),
-        chunkFilename: utils.assetsPath('js/[name].[chunkhash].js')
-    },
-    optimization: {
-        // split vendor js into its own file
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    chunks: 'initial',
-                    name: 'vendor'
-                }
-            }
-        },
-        minimizer: [
-            new UglifyJsPlugin({
-                sourceMap: config.build.productionSourceMap,
-                cache: true,
-                parallel: true,
-                uglifyOptions: {
-                    compress: {
-                        warnings: false
-                    },
-                    comments: false,
-                    sourceMap: true
-                }
-            })
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': env
-        }),
-        // extract css into its own file
-        new MiniCssExtractPlugin({
-            filename: utils.assetsPath('css/[name].[contenthash].css')
-        }),
-        // generate dist index.html with correct asset hash for caching.
-        // you can customize output by editing /index.html
-        // see https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: config.build.index,
-            template: 'index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-                // more options:
-                // https://github.com/kangax/html-minifier#options-quick-reference
-            },
-            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-            chunksSortMode: 'dependency'
-        }),
-        // copy custom static assets
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, '../static'),
-                to: config.build.assetsSubDirectory,
-                ignore: ['.*']
-            }
-        ])
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: config.build.productionSourceMap,
+        cache: true,
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          warnings: false,
+          output: {
+            comments: false
+          }
+        }
+      })
     ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": env
+    }),
+    // extract css into its own file
+    new MiniCssExtractPlugin({
+      filename: utils.assetsPath("css/[name].[contenthash].css")
+    }),
+    // generate dist index.html with correct asset hash for caching.
+    // you can customize output by editing /index.html
+    // see https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: config.build.index,
+      template: "index.html",
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: "dependency"
+    }),
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "../static"),
+        to: config.build.assetsSubDirectory,
+        ignore: [".*"]
+      }
+    ])
+  ]
 });
 
 if (config.build.productionGzip) {
-    const CompressionWebpackPlugin = require('compression-webpack-plugin');
+  const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
-    webpackConfig.plugins.push(
-        new CompressionWebpackPlugin({
-            asset: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: new RegExp(
-                '\\.(' +
-                config.build.productionGzipExtensions.join('|') +
-                ')$'
-            ),
-            threshold: 10240,
-            minRatio: 0.8
-        })
-    );
+  webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: new RegExp(
+        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
+      ),
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  );
 }
 
 if (config.build.bundleAnalyzerReport) {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-    webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
 module.exports = webpackConfig;
